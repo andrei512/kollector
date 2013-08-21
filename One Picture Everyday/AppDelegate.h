58be24bc46800b567649f0db9d3e4879dea08c56
Andrei Puni <puni.andrei23@gmail.com>
2013-08-21 20:29:07 +0300

    Initial Commit

---
+++ One Picture Everyday/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  One Picture Everyday
+//
+//  Created by Andrei on 8/21/13.
+//  Copyright (c) 2013 Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
